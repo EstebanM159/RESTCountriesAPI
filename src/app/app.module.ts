184c8c5223c09a900554d02960ab57ad9ac80551
EstebanM159 <estebanm91231@gmail.com>
2023-12-12 10:32:57 -0300

    Se agregaron los componentes necesarios para el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
+import { CountryDetailComponent } from './components/country-detail/country-detail.component';
+import { SearchComponent } from './components/search/search.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    CountryListComponent,
+    CountryDetailComponent,
+    SearchComponent,
+    FilterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
